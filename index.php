f0f706965cd2e3cbaca8c492026be98a60eab060
nikbobo <nikbobos@gmail.com>
2014-05-24 10:38:41 +0800

    失败，推倒重来

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * User: 永强
- * Date: 2014/5/22
- * Time: 20:12
- */
-define('APPLICATION_PATH', dirname(__FILE__)); // 定义应用根目录
-require APPLICATION_PATH . '/Framework/Init.php'; // 导入框架核心文件
